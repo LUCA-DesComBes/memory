92eb6f7c76237e614578a2f9a473c3b7e4a69e86
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-01-12 15:39:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let card = document.querySelectorAll('div');
+card = addEventListener("click", () => {
+  console.log('clicker')
+})
+let game_returned = 0;
+let user_returned = 0;
+
+if(user_returned < 2) {
+  
+}
